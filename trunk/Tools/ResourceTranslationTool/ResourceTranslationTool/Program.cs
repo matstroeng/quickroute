ed7b124295db5c0d78cfe36fe6ae5d48b0087489
matstroeng <matstroeng@c56ce352-d6d6-11de-96bf-fbce4fd84a6e>
2009-11-22 23:49:22 +0000

    Added download functionality to the ResourceTranslationTool.

--- trunk/Tools/ResourceTranslationTool/ResourceTranslationTool/Program.cs
+++ trunk/Tools/ResourceTranslationTool/ResourceTranslationTool/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using Ionic.Zip;
 
 namespace QuickRoute.Tools.ResourceTranslationTool
 {
@@ -10,23 +12,31 @@ namespace QuickRoute.Tools.ResourceTranslationTool
   {
     static void Main(string[] args)
     {
-      var solutionPath = args[0];
-      var outputPath = args[1];
+      var action = args[0];
+
+      if (action == "-upload") Upload(args);
+      if (action == "-download") Download(args);
+    }
+
+    private static void Upload(string[] args)
+    {
+      var solutionPath = args[1];
+      var outputPath = args[2];
 
       var solution = new Solution(solutionPath);
       var languages = solution.GetLanguages();
 
-      foreach(var language in languages)
+      foreach (var language in languages)
       {
         var sb = new StringBuilder();
         sb.AppendLine("Created at " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ".");
         sb.AppendLine();
 
-        foreach(var file in solution.GetLocalizedResourceFiles(language))
+        foreach (var file in solution.GetLocalizedResourceFiles(language))
         {
           var fileNameWithoutSolutionPath = file.FileName.Replace(solutionPath, "");
           var untranslatedResources = file.GetUntranslatedResourceKeys();
-          if(untranslatedResources.Count() > 0)
+          if (untranslatedResources.Count() > 0)
           {
             Console.WriteLine("Processing {0} ({1} untranslated resources)...", fileNameWithoutSolutionPath, untranslatedResources.Count());
             sb.AppendLine("=========================================================================");
@@ -40,5 +50,40 @@ namespace QuickRoute.Tools.ResourceTranslationTool
         File.WriteAllText(Path.Combine(outputPath, language + ".txt"), sb.ToString());
       }
     }
+
+    private static void Download(string[] args)
+    {
+      var solutionPath = args[1];
+      var serverUrl = args[2];
+
+      // get new files
+      var client = new WebClient();
+      var newFiles = client.DownloadString(serverUrl + "?action=getNewUploads");
+
+      if(!string.IsNullOrEmpty(newFiles))
+      {
+        foreach (var data in newFiles.Split('\n'))
+        {
+          var atoms = data.Split(';');
+          var id = atoms[0];
+          var language = atoms[1];
+          var extension = "." + language + ".resx";
+          var zipFileData = client.DownloadData(serverUrl + "?action=downloadZipFile&id=" + id);
+          var zipFile = ZipFile.Read(zipFileData);
+          foreach (var file in zipFile)
+          {
+            // only extract files with the .[language].resx extension
+            if (file.FileName.EndsWith(extension))
+            {
+              Console.WriteLine("Extracting {0}...", file.FileName);
+              file.Extract(solutionPath, true);
+              // make sure to add the file to the subversion repository
+              SvnManager.AddFile(Path.Combine(solutionPath, file.FileName));
+            }
+          }
+        }
+      }
+    }
+  
   }
 }
