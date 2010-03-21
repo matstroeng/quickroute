12f54675cab09d655ca2533cdcd9570a8590ca51
matstroeng <matstroeng@c56ce352-d6d6-11de-96bf-fbce4fd84a6e>
2010-03-21 13:14:25 +0000

    Committed some files that was forgotten last time, and thus broke the build

---
+++ trunk/QuickRoute.UI/Forms/RefreshDevicesProgressIndicator.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace QuickRoute.UI.Forms
+{
+    partial class RefreshDevicesProgressIndicator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RefreshDevicesProgressIndicator));
+            this.status = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // status
+            // 
+            resources.ApplyResources(this.status, "status");
+            this.status.Name = "status";
+            // 
+            // progressBar
+            // 
+            resources.ApplyResources(this.progressBar, "progressBar");
+            this.progressBar.Name = "progressBar";
+            // 
+            // cancel
+            // 
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.cancel, "cancel");
+            this.cancel.Name = "cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // RefreshDevicesProgressIndicator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ControlBox = false;
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.status);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RefreshDevicesProgressIndicator";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button cancel;
+    }
+}
---
+++ trunk/QuickRoute.UI/Forms/RefreshDevicesProgressIndicator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using QuickRoute.BusinessEntities.Importers;
+
+namespace QuickRoute.UI.Forms
+{
+    public partial class RefreshDevicesProgressIndicator : Form
+    {
+        public RefreshDevicesProgressIndicator()
+        {
+            InitializeComponent();
+            SupportedImportFormatManager.RefreshProgressChanged += RefreshProgressChanged;
+            SupportedImportFormatManager.RefreshCompleted += RefreshCompleted;
+            status.Text = "";
+        }
+
+        void RefreshProgressChanged(string message, int percent)
+        {
+            try
+            {
+                if (InvokeRequired)
+                {
+                    var arg = new object[2];
+                    arg[0] = message;
+                    arg[1] = percent;
+                    BeginInvoke(new RefreshProgressDelegate(RefreshProgressChanged), arg);
+                    return;
+                }
+                status.Text = message;
+                progressBar.Value = percent;
+            }
+            catch (Exception)
+            {
+                // do nothing
+            }
+        }
+        void RefreshCompleted()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new RefreshCompletedDelegate(RefreshCompleted));
+                return;
+            }
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // TODO: implement refresh cancel
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
