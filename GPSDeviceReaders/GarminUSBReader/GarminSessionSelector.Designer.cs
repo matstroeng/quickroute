d7ad6075cbb9f491799cc655a7bf261bf7156dad
matstroeng <matstroeng@c56ce352-d6d6-11de-96bf-fbce4fd84a6e>
2010-08-09 22:25:31 +0000

    Adjusted Italian UI label widths.

--- GPSDeviceReaders/GarminUSBReader/GarminSessionSelector.Designer.cs
+++ GPSDeviceReaders/GarminUSBReader/GarminSessionSelector.Designer.cs
@@ -28,66 +28,90 @@ namespace QuickRoute.GPSDeviceReaders.GarminUSBReader
     /// </summary>
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GarminSessionSelector));
-        this.ok = new System.Windows.Forms.Button();
-        this.activityLabel = new System.Windows.Forms.Label();
-        this.sessionsComboBox = new System.Windows.Forms.ComboBox();
-        this.cancel = new System.Windows.Forms.Button();
-        this.readGPSUnit = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-        // 
-        // ok
-        // 
-        resources.ApplyResources(this.ok, "ok");
-        this.ok.Name = "ok";
-        this.ok.UseVisualStyleBackColor = true;
-        this.ok.Click += new System.EventHandler(this.ok_Click);
-        // 
-        // activityLabel
-        // 
-        resources.ApplyResources(this.activityLabel, "activityLabel");
-        this.activityLabel.Name = "activityLabel";
-        // 
-        // sessionsComboBox
-        // 
-        this.sessionsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-        this.sessionsComboBox.FormattingEnabled = true;
-        resources.ApplyResources(this.sessionsComboBox, "sessionsComboBox");
-        this.sessionsComboBox.Name = "sessionsComboBox";
-        // 
-        // cancel
-        // 
-        this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        resources.ApplyResources(this.cancel, "cancel");
-        this.cancel.Name = "cancel";
-        this.cancel.UseVisualStyleBackColor = true;
-        this.cancel.Click += new System.EventHandler(this.cancel_Click);
-        // 
-        // readGPSUnit
-        // 
-        resources.ApplyResources(this.readGPSUnit, "readGPSUnit");
-        this.readGPSUnit.Name = "readGPSUnit";
-        this.readGPSUnit.UseVisualStyleBackColor = true;
-        this.readGPSUnit.Click += new System.EventHandler(this.readGPSUnit_Click);
-        // 
-        // GarminSessionSelector
-        // 
-        this.AcceptButton = this.ok;
-        resources.ApplyResources(this, "$this");
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.CancelButton = this.cancel;
-        this.Controls.Add(this.readGPSUnit);
-        this.Controls.Add(this.cancel);
-        this.Controls.Add(this.sessionsComboBox);
-        this.Controls.Add(this.activityLabel);
-        this.Controls.Add(this.ok);
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
-        this.Name = "GarminSessionSelector";
-        this.ShowIcon = false;
-        this.ShowInTaskbar = false;
-        this.ResumeLayout(false);
-        this.PerformLayout();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GarminSessionSelector));
+      this.ok = new System.Windows.Forms.Button();
+      this.activityLabel = new System.Windows.Forms.Label();
+      this.sessionsComboBox = new System.Windows.Forms.ComboBox();
+      this.cancel = new System.Windows.Forms.Button();
+      this.readGPSUnit = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // ok
+      // 
+      this.ok.AccessibleDescription = null;
+      this.ok.AccessibleName = null;
+      resources.ApplyResources(this.ok, "ok");
+      this.ok.BackgroundImage = null;
+      this.ok.Font = null;
+      this.ok.Name = "ok";
+      this.ok.UseVisualStyleBackColor = true;
+      this.ok.Click += new System.EventHandler(this.ok_Click);
+      // 
+      // activityLabel
+      // 
+      this.activityLabel.AccessibleDescription = null;
+      this.activityLabel.AccessibleName = null;
+      resources.ApplyResources(this.activityLabel, "activityLabel");
+      this.activityLabel.Font = null;
+      this.activityLabel.Name = "activityLabel";
+      // 
+      // sessionsComboBox
+      // 
+      this.sessionsComboBox.AccessibleDescription = null;
+      this.sessionsComboBox.AccessibleName = null;
+      resources.ApplyResources(this.sessionsComboBox, "sessionsComboBox");
+      this.sessionsComboBox.BackgroundImage = null;
+      this.sessionsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.sessionsComboBox.Font = null;
+      this.sessionsComboBox.FormattingEnabled = true;
+      this.sessionsComboBox.Name = "sessionsComboBox";
+      // 
+      // cancel
+      // 
+      this.cancel.AccessibleDescription = null;
+      this.cancel.AccessibleName = null;
+      resources.ApplyResources(this.cancel, "cancel");
+      this.cancel.BackgroundImage = null;
+      this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.cancel.Font = null;
+      this.cancel.Name = "cancel";
+      this.cancel.UseVisualStyleBackColor = true;
+      this.cancel.Click += new System.EventHandler(this.cancel_Click);
+      // 
+      // readGPSUnit
+      // 
+      this.readGPSUnit.AccessibleDescription = null;
+      this.readGPSUnit.AccessibleName = null;
+      resources.ApplyResources(this.readGPSUnit, "readGPSUnit");
+      this.readGPSUnit.BackgroundImage = null;
+      this.readGPSUnit.Font = null;
+      this.readGPSUnit.Name = "readGPSUnit";
+      this.readGPSUnit.UseVisualStyleBackColor = true;
+      this.readGPSUnit.Click += new System.EventHandler(this.readGPSUnit_Click);
+      // 
+      // GarminSessionSelector
+      // 
+      this.AcceptButton = this.ok;
+      this.AccessibleDescription = null;
+      this.AccessibleName = null;
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = null;
+      this.CancelButton = this.cancel;
+      this.Controls.Add(this.readGPSUnit);
+      this.Controls.Add(this.cancel);
+      this.Controls.Add(this.sessionsComboBox);
+      this.Controls.Add(this.activityLabel);
+      this.Controls.Add(this.ok);
+      this.Font = null;
+      this.Icon = null;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "GarminSessionSelector";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
--- GPSDeviceReaders/JJConnectRegistratorSEReader/JJConnectRegistratorSETrackSelector.Designer.cs
+++ GPSDeviceReaders/JJConnectRegistratorSEReader/JJConnectRegistratorSETrackSelector.Designer.cs
@@ -28,76 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cbSelectedTrack = new System.Windows.Forms.ComboBox();
-            this.lblSelectedTarck = new System.Windows.Forms.Label();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnScan = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // cbSelectedTrack
-            // 
-            this.cbSelectedTrack.FormattingEnabled = true;
-            this.cbSelectedTrack.Location = new System.Drawing.Point(12, 29);
-            this.cbSelectedTrack.Name = "cbSelectedTrack";
-            this.cbSelectedTrack.Size = new System.Drawing.Size(473, 24);
-            this.cbSelectedTrack.TabIndex = 12;
-            // 
-            // lblSelectedTarck
-            // 
-            this.lblSelectedTarck.AutoSize = true;
-            this.lblSelectedTarck.Location = new System.Drawing.Point(12, 9);
-            this.lblSelectedTarck.Name = "lblSelectedTarck";
-            this.lblSelectedTarck.Size = new System.Drawing.Size(62, 17);
-            this.lblSelectedTarck.TabIndex = 13;
-            this.lblSelectedTarck.Text = "Session:";
-            // 
-            // btnOk
-            // 
-            this.btnOk.Location = new System.Drawing.Point(259, 70);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(110, 30);
-            this.btnOk.TabIndex = 14;
-            this.btnOk.Text = "OK";
-            this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(375, 70);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(110, 30);
-            this.btnCancel.TabIndex = 15;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // btnScan
-            // 
-            this.btnScan.Location = new System.Drawing.Point(87, 70);
-            this.btnScan.Name = "btnScan";
-            this.btnScan.Size = new System.Drawing.Size(166, 30);
-            this.btnScan.TabIndex = 1;
-            this.btnScan.Text = "Re-read GPS unit";
-            this.btnScan.UseVisualStyleBackColor = true;
-            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
-            // 
-            // GlobalSatGH615MTrackSelector
-            // 
-            this.AcceptButton = this.btnOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 116);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnScan);
-            this.Controls.Add(this.btnOk);
-            this.Controls.Add(this.lblSelectedTarck);
-            this.Controls.Add(this.cbSelectedTrack);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "GlobalSatGH615MTrackSelector";
-            this.Text = "Select session";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+          System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JJConnectRegistratorSETrackSelector));
+          this.cbSelectedTrack = new System.Windows.Forms.ComboBox();
+          this.lblSelectedTarck = new System.Windows.Forms.Label();
+          this.btnOk = new System.Windows.Forms.Button();
+          this.btnCancel = new System.Windows.Forms.Button();
+          this.btnScan = new System.Windows.Forms.Button();
+          this.SuspendLayout();
+          // 
+          // cbSelectedTrack
+          // 
+          this.cbSelectedTrack.AccessibleDescription = null;
+          this.cbSelectedTrack.AccessibleName = null;
+          resources.ApplyResources(this.cbSelectedTrack, "cbSelectedTrack");
+          this.cbSelectedTrack.BackgroundImage = null;
+          this.cbSelectedTrack.Font = null;
+          this.cbSelectedTrack.FormattingEnabled = true;
+          this.cbSelectedTrack.Name = "cbSelectedTrack";
+          // 
+          // lblSelectedTarck
+          // 
+          this.lblSelectedTarck.AccessibleDescription = null;
+          this.lblSelectedTarck.AccessibleName = null;
+          resources.ApplyResources(this.lblSelectedTarck, "lblSelectedTarck");
+          this.lblSelectedTarck.Font = null;
+          this.lblSelectedTarck.Name = "lblSelectedTarck";
+          // 
+          // btnOk
+          // 
+          this.btnOk.AccessibleDescription = null;
+          this.btnOk.AccessibleName = null;
+          resources.ApplyResources(this.btnOk, "btnOk");
+          this.btnOk.BackgroundImage = null;
+          this.btnOk.Font = null;
+          this.btnOk.Name = "btnOk";
+          this.btnOk.UseVisualStyleBackColor = true;
+          this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+          // 
+          // btnCancel
+          // 
+          this.btnCancel.AccessibleDescription = null;
+          this.btnCancel.AccessibleName = null;
+          resources.ApplyResources(this.btnCancel, "btnCancel");
+          this.btnCancel.BackgroundImage = null;
+          this.btnCancel.Font = null;
+          this.btnCancel.Name = "btnCancel";
+          this.btnCancel.UseVisualStyleBackColor = true;
+          this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+          // 
+          // btnScan
+          // 
+          this.btnScan.AccessibleDescription = null;
+          this.btnScan.AccessibleName = null;
+          resources.ApplyResources(this.btnScan, "btnScan");
+          this.btnScan.BackgroundImage = null;
+          this.btnScan.Font = null;
+          this.btnScan.Name = "btnScan";
+          this.btnScan.UseVisualStyleBackColor = true;
+          this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+          // 
+          // JJConnectRegistratorSETrackSelector
+          // 
+          this.AcceptButton = this.btnOk;
+          this.AccessibleDescription = null;
+          this.AccessibleName = null;
+          resources.ApplyResources(this, "$this");
+          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+          this.BackgroundImage = null;
+          this.Controls.Add(this.btnCancel);
+          this.Controls.Add(this.btnScan);
+          this.Controls.Add(this.btnOk);
+          this.Controls.Add(this.lblSelectedTarck);
+          this.Controls.Add(this.cbSelectedTrack);
+          this.Font = null;
+          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+          this.Icon = null;
+          this.Name = "JJConnectRegistratorSETrackSelector";
+          this.ResumeLayout(false);
+          this.PerformLayout();
 
         }
 
