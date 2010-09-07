78f1a16d2d8d4f2a6c55f471c5877a2b12d586e6
matstroeng <matstroeng@c56ce352-d6d6-11de-96bf-fbce4fd84a6e>
2010-09-07 20:30:16 +0000

    Corrected path to Polar Pro Trainer software on 64 bit systems.

--- QuickRoute.BusinessEntities/Importers/SupportedImportFormatManager.cs
+++ QuickRoute.BusinessEntities/Importers/SupportedImportFormatManager.cs
@@ -50,7 +50,7 @@ namespace QuickRoute.BusinessEntities.Importers
       var polarProTrainerImporter = new PolarProTrainerImporter();
       polarProTrainerImporter.Paths = new List<string>
                                         {
-                                          Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\Polar\Polar ProTrainer\",
+                                          GetProgramFilesPath() + @"\Polar\Polar ProTrainer\",
                                           Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\VirtualStore\Program Files\Polar\Polar ProTrainer\",
                                         };
       GPSDevice polarProTrainerDevice = new GPSDevice(polarProTrainerImporter);
@@ -166,6 +166,16 @@ namespace QuickRoute.BusinessEntities.Importers
             RefreshCompleted();
         }
     }
+
+    private static string GetProgramFilesPath()
+    {
+      if (IntPtr.Size == 8 || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
+      {
+        return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+      }
+      return Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+    }
+
   }
 
   public class FileFormat
